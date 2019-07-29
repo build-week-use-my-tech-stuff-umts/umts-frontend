c9af8de79f2b2543fdfb62b623f8d54f66123301
Skyler <skyler2440@gmail.com>
2019-07-29 14:27:58 -0400

    New Item form
    Needs to pull props from profile to populate the value of address

---
+++ forms/NewItemForm.js
@@ -0,0 +1,90 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
+// import axios from "axios";
+
+function NewItmFrm({ values, errors, touched, isSubmitting }) {
+  return (
+    
+    <Form>
+    <label htmlFor='itemType'> Type of Item:
+      <Field component="select" name="itemType">
+          <option value="Camera">Camera</option>
+          <option value="TV">TV</option>
+          <option value="Audio">Audio</option>
+          <option value="Computers">Computer</option>
+          <option value="Others">Other</option>
+        </Field>
+    </label>
+      <div>
+        {touched.description && errors.description && <p>{errors.description}</p>}
+        <Field component='textarea' name="description" placeholder="Item Description" />
+      </div>
+      <div>
+        {touched.price && errors.price && <p>{errors.price}</p>}
+        <Field type="text" name="price" placeholder="Weekly Rental Price" />
+      </div>
+      <div>
+        {touched.imageUrl && errors.imageUrl && <p>{errors.imageUrl}</p>}
+        <Field type="url" name="imageUrl" placeholder="Product Image URL" />
+      </div>
+      <div>
+        {touched.address && errors.address && <p>{errors.address}</p>}
+        <Field type="text" name="address" placeholder="Address" />
+        {/* Address value should populate with address in the persons profile */}
+      </div>
+      
+      {/* disabled={isSubmitting}  ***Removed from submit button for testing***/}
+      <button type='submit'>Submit</button>
+      <button type='reset'>Reset Form</button>
+    </Form>
+  );
+}
+
+const NewItemForm = withFormik({
+  mapPropsToValues({
+    itemType,
+    description,
+    price,
+    imageUrl,
+    address
+    
+  }) {
+    return {
+      itemType: itemType || "Camera",
+      description: description || "",
+      price: price || "",
+     imageUrl: imageUrl || "",
+      address: address || ""
+    };
+  },
+  validationSchema: Yup.object().shape({
+    itemType: Yup.string().required("Type is required"),
+    description: Yup.string().required("Description is required"),
+    price: Yup.string()
+      .required("Price is required"),
+    imageUrl: Yup.string()
+    .url('Must be a valid URL')
+     .required("Image URL is required"),
+  }),
+
+  handleSubmit(values, { resetForm, setErrors, setSubmitting }) {
+      //   axios
+      //     .post("https://yourdatabaseurlgoeshere.com", values)
+      //     .then(res => {
+      //       console.log(res); // Data was created successfully and logs to console
+      //       resetForm();
+      //       setSubmitting(false);
+      //     })
+      //     .catch(err => {
+      //       console.log(err); // There was an error creating the data and logs to console
+      //       setSubmitting(false);
+      //     });
+      console.log(
+        values
+      );
+    }
+  }
+)(NewItmFrm);
+export default NewItemForm;
