8c47ec0aadc3652f497d558a231c87059f615547
Wes Jonke <wesjonke@gmail.com>
2019-07-29 13:57:00 -0400

    added API folder with withoutAuth and withAuth func.

--- rentech/src/App.js
+++ rentech/src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className="App">
       
+
+
+      
     </div>
   );
 }
---
+++ rentech/src/api/umtsApi.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+
+
+export default axios.create({
+	baseURL : 'https://umts-backend.herokuapp.com/api',
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
