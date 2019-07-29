6a0ddd3b07f29aee38a4ba55535e0b5607f5fbb3
Adnan <adnan.chowdhury@outlook.com>
2019-07-29 13:39:29 -0500

    Breaking components down for better clarity, installed SASS

--- rentech/src/components/render/ProfileHeader.js
+++ rentech/src/components/render/ProfileHeader.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const ProfileHeader = props => {
-  return <div>Profile header</div>;
+  return (
+    <div className="profile-header">
+      <div className="logo">LOGO RIGHT HERE</div>
+      <h1>RenTech</h1>
+    </div>
+  );
 };
 
 export default ProfileHeader;
