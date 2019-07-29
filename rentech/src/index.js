ec3638ce5a73d0af2ac089167d762f7ce23642a5
Adnan <adnan.chowdhury@outlook.com>
2019-07-29 13:41:00 -0500

    css converted to scss

--- rentech/src/index.js
+++ rentech/src/index.js
@@ -1,29 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { applyMiddleware, createStore } from 'redux';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { applyMiddleware, createStore } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 
-import rootReducer from './store/reducers/index';
-import { authMiddleware } from './customMiddleware/authMiddleware';
-import App from './App';
+import rootReducer from "./store/reducers/index";
+import { authMiddleware } from "./customMiddleware/authMiddleware";
+import App from "./App";
 
-import './index.css';
-
-
-
-
-
+import "./index.scss";
 
 //const store = createStore(rootReducer, applyMiddleware(thunk, authMiddleware));
 
 ReactDOM.render(
   <Router>
-      <App />
-    </Router>
-  ,
-  document.getElementById('root'),
+    <App />
+  </Router>,
+  document.getElementById("root")
 );
-
-
