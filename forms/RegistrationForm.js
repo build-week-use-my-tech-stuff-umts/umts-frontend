8ca885781e97d33f19a36c2138e5c6abd7b3fc05
Skyler <skyler2440@gmail.com>
2019-07-29 16:36:28 -0400

    Reg form styled for semantic ui

--- forms/RegistrationForm.js
+++ forms/RegistrationForm.js
@@ -6,109 +6,150 @@ import * as Yup from "yup";
 
 function RegFrm({ values, errors, touched, isSubmitting }) {
   return (
-    <Form>
-      <div>
-        {touched.firstName && errors.firstName && <p>{errors.firstName}</p>}
-        <Field type="text" name="firstName" placeholder="First Name" />
-      </div>
-      <div>
-        {touched.lastName && errors.lastName && <p>{errors.lastName}</p>}
-        <Field type="text" name="lastName" placeholder="Last Name" />
-      </div>
-      <div>
-        {touched.email && errors.email && <p>{errors.email}</p>}
-        <Field type="email" name="email" placeholder="Email" />
-      </div>
-      <div>
-        {touched.phone && errors.phone && <p>{errors.phone}</p>}
-        <Field type="phone" name="phone" placeholder="(555) 555-5555" />
-      </div>
-      <div>
-        {touched.address && errors.address && <p>{errors.address}</p>}
-        <Field type="text" name="address" placeholder="Address" />
-      </div>
-      <div>
-        {touched.city && errors.city && <p>{errors.city}</p>}
-        <Field type="text" name="city" placeholder="City" />
-      </div>
-      <div>
-        <Field component="select" name="state">
-          <option value="AK">Alaska</option>
-          <option value="AZ">Arizona</option>
-          <option value="AR">Arkansas</option>
-          <option value="CA">California</option>
-          <option value="CO">Colorado</option>
-          <option value="CT">Connecticut</option>
-          <option value="DE">Delaware</option>
-          <option value="FL">Florida</option>
-          <option value="GA">Georgia</option>
-          <option value="HI">Hawaii</option>
-          <option value="ID">Idaho</option>
-          <option value="IL">Illinois</option>
-          <option value="IN">Indiana</option>
-          <option value="IA">Iowa</option>
-          <option value="KS">Kansas</option>
-          <option value="KY">Kentucky</option>
-          <option value="LA">Louisiana</option>
-          <option value="ME">Maine</option>
-          <option value="MD">Maryland</option>
-          <option value="MA">Massachusetts</option>
-          <option value="MI">Michigan</option>
-          <option value="MN">Minnesota</option>
-          <option value="MS">Mississippi</option>
-          <option value="MO">Missouri</option>
-          <option value="MT">Montana</option>
-          <option value="NE">Nebraska</option>
-          <option value="NV">Nevada</option>
-          <option value="NH">New Hampshire</option>
-          <option value="NJ">New Jersey</option>
-          <option value="NM">New Mexico</option>
-          <option value="NY">New York</option>
-          <option value="NC">North Carolina</option>
-          <option value="ND">North Dakota</option>
-          <option value="OH">Ohio</option>
-          <option value="OK">Oklahoma</option>
-          <option value="OR">Oregon</option>
-          <option value="PA">Pennsylvania</option>
-          <option value="RI">Rhode Island</option>
-          <option value="SC">South Carolina</option>
-          <option value="SD">South Dakota</option>
-          <option value="TN">Tennessee</option>
-          <option value="TX">Texas</option>
-          <option value="UT">Utah</option>
-          <option value="VT">Vermont</option>
-          <option value="VA">Virginia</option>
-          <option value="WA">Washington</option>
-          <option value="WV">West Virginia</option>
-          <option value="WI">Wisconsin</option>
-          <option value="WY">Wyoming</option>
-        </Field>
-      </div>
-      <div>
-        {touched.zip && errors.zip && <p>{errors.zip}</p>}
-        <Field type="text" name="zip" placeholder="Zip" />
-      </div>
-      <div>
-        {touched.password && errors.password && <p>{errors.password}</p>}
-        <Field type="password" name="password" placeholder="Password" />
-      </div>
-      <div>
-        {touched.cPassword && errors.cPassword && <p>{errors.cPassword}</p>}
-        <Field
-          type="password"
-          name="cPassword"
-          placeholder="Confirm Password"
-        />
-      </div>
-      <label>
-        {touched.tos && errors.tos && <p>{errors.tos}</p>}
-        <Field type="checkbox" name="tos" checked={values.tos} />
-        Accept TOS
-      </label>
-      {/* disabled={isSubmitting}  ***Removed from submit button for testing***/}
-      <button type='submit'>Submit</button>
-      <button type='reset'>Reset Form</button>
-    </Form>
+    <div className="form-card">
+      <Form className="ui form">
+        <div className="field">
+          <label htmlFor="firstName">
+            First Name
+            {touched.firstName && errors.firstName && <p>{errors.firstName}</p>}
+            <Field type="text" name="firstName" placeholder="First Name" />
+          </label>
+        </div>
+        <div className="field">
+          <label htmlFor="lastName">
+            Last Name
+            {touched.lastName && errors.lastName && <p>{errors.lastName}</p>}
+            <Field type="text" name="lastName" placeholder="Last Name" />
+          </label>
+        </div>
+        <div className="field">
+          <label htmlFor="email">
+            Email
+            {touched.email && errors.email && <p>{errors.email}</p>}
+            <Field type="email" name="email" placeholder="Email" />
+          </label>
+        </div>
+        <div className="field">
+          <label htmlFor="phone">
+            {" "}
+            Phone
+            {touched.phone && errors.phone && <p>{errors.phone}</p>}
+            <Field type="text" name="phone" placeholder="(555) 555-5555" />
+          </label>
+        </div>
+        <div className="field">
+          <label htmlFor="address">
+            {" "}
+            Address
+            {touched.address && errors.address && <p>{errors.address}</p>}
+            <Field type="text" name="address" placeholder="Address" />
+          </label>
+        </div>
+        <div className="field">
+          <label htmlFor="city">
+            {" "}
+            City
+            {touched.city && errors.city && <p>{errors.city}</p>}
+            <Field type="text" name="city" placeholder="City" />
+          </label>
+        </div>
+        <div className="field">
+          <label htmlFor="state">
+            State
+            <Field component="select" name="state">
+              <option value="AK">Alaska</option>
+              <option value="AZ">Arizona</option>
+              <option value="AR">Arkansas</option>
+              <option value="CA">California</option>
+              <option value="CO">Colorado</option>
+              <option value="CT">Connecticut</option>
+              <option value="DE">Delaware</option>
+              <option value="FL">Florida</option>
+              <option value="GA">Georgia</option>
+              <option value="HI">Hawaii</option>
+              <option value="ID">Idaho</option>
+              <option value="IL">Illinois</option>
+              <option value="IN">Indiana</option>
+              <option value="IA">Iowa</option>
+              <option value="KS">Kansas</option>
+              <option value="KY">Kentucky</option>
+              <option value="LA">Louisiana</option>
+              <option value="ME">Maine</option>
+              <option value="MD">Maryland</option>
+              <option value="MA">Massachusetts</option>
+              <option value="MI">Michigan</option>
+              <option value="MN">Minnesota</option>
+              <option value="MS">Mississippi</option>
+              <option value="MO">Missouri</option>
+              <option value="MT">Montana</option>
+              <option value="NE">Nebraska</option>
+              <option value="NV">Nevada</option>
+              <option value="NH">New Hampshire</option>
+              <option value="NJ">New Jersey</option>
+              <option value="NM">New Mexico</option>
+              <option value="NY">New York</option>
+              <option value="NC">North Carolina</option>
+              <option value="ND">North Dakota</option>
+              <option value="OH">Ohio</option>
+              <option value="OK">Oklahoma</option>
+              <option value="OR">Oregon</option>
+              <option value="PA">Pennsylvania</option>
+              <option value="RI">Rhode Island</option>
+              <option value="SC">South Carolina</option>
+              <option value="SD">South Dakota</option>
+              <option value="TN">Tennessee</option>
+              <option value="TX">Texas</option>
+              <option value="UT">Utah</option>
+              <option value="VT">Vermont</option>
+              <option value="VA">Virginia</option>
+              <option value="WA">Washington</option>
+              <option value="WV">West Virginia</option>
+              <option value="WI">Wisconsin</option>
+              <option value="WY">Wyoming</option>
+            </Field>
+          </label>
+        </div>
+        <div className="field">
+          <label htmlFor="zip">
+            Zip
+            {touched.zip && errors.zip && <p>{errors.zip}</p>}
+            <Field type="text" name="zip" placeholder="Zip" />
+          </label>
+        </div>
+        <div className="field">
+          <label htmlFor="password">
+            Password
+            {touched.password && errors.password && <p>{errors.password}</p>}
+            <Field type="password" name="password" placeholder="Password" />
+          </label>
+        </div>
+        <div className="field">
+          <label htmlFor="cPassword">
+            Confirm Password
+            {touched.cPassword && errors.cPassword && <p>{errors.cPassword}</p>}
+            <Field
+              type="password"
+              name="cPassword"
+              placeholder="Confirm Password"
+            />
+          </label>
+        </div>
+        <div className="field">
+          <label htmlFor="tos">
+            {touched.tos && errors.tos && <p>{errors.tos}</p>}
+            <Field type="checkbox" name="tos" checked={values.tos} />
+            Accept TOS
+          </label>
+        </div>
+        {/* disabled={isSubmitting}  ***Removed from submit button for testing***/}
+        <button className="ui button" type="submit">
+          Submit
+        </button>
+        <button className="ui button" type="reset">
+          Reset Form
+        </button>
+      </Form>
+    </div>
   );
 }
 
@@ -149,10 +190,13 @@ const RegistrationForm = withFormik({
     password: Yup.string()
       .min(8, "Password must be 8 characters or longer")
       .required("Password is required"),
-      cPassword: Yup.string().oneOf([Yup.ref('password'), null], 'Passwords must match!'),
+    cPassword: Yup.string().oneOf(
+      [Yup.ref("password"), null],
+      "Passwords must match!"
+    ),
     zip: Yup.string()
       .min(5, "Zipcode must be 5 numbers")
-      .max(5, "Zipcode cannot be longer than 5 numbers"),
+      .max(5, "Zipcode cannot be longer than 5 numbers")
   }),
 
   handleSubmit(values, { resetForm, setErrors, setSubmitting }) {
@@ -172,9 +216,7 @@ const RegistrationForm = withFormik({
       //       console.log(err); // There was an error creating the data and logs to console
       //       setSubmitting(false);
       //     });
-      console.log(
-        values
-      );
+      console.log(values);
     }
   }
 })(RegFrm);
