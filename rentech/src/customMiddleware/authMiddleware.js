25301662fac4d55135414fcc88a8310612768bba
Wes Jonke <wesjonke@gmail.com>
2019-07-29 12:38:20 -0400

    added action types

--- rentech/src/customMiddleware/authMiddleware.js
+++ rentech/src/customMiddleware/authMiddleware.js
@@ -1,14 +1,14 @@
-import types from '../store/actions/index';
+import {types} from '../store/actions/index';
 
 
 
 
 export const authMiddleware = state => next => action => {
 	if (action.type === types.LOGIN_SUCCESS) {
-		localStorage.setItem('TBD', JSON.stringify(action.payload.token));
+		localStorage.setItem('token', JSON.stringify(action.payload.token));
 	}
 	if (action.type === types.LOGOUT_SUCCESS) {
-		localStorage.removeItem('TBD');
+		localStorage.removeItem('token');
 	}
 
 	next(action);
