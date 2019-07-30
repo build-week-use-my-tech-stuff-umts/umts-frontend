e663f579bcc6c3ae61bce7eec71ff1c88305c3ba
Skyler Dowdy <skylerwebdev@gmail.com>
2019-07-30 18:59:03 +0000

    Fixed account page

--- rentech/src/components/render/AccountInfo.js
+++ rentech/src/components/render/AccountInfo.js
@@ -4,14 +4,15 @@ const AccountInfo = props => {
   return (
     <div className="account">
       {/* Plug in the user details in respective forms */}
-      <h2>Account Information</h2>
+     <div className='account-header'> <h2>Account Information</h2>
+       <EditAccount />
+    </div>
       <div>Name: John Doe</div>
       <div>Address: 123 Waywerd lane</div>
       <div>State: TX</div>
       <div>City: Austin</div>
       <div>Phone: (111)222-3333</div>
       <div>Password: ********</div>
-      <EditAccount />
     </div>
   );
 };
