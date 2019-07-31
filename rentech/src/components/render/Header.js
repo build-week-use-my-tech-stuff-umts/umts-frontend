56cfbe6121c9e23864829a3dca9285cc74f83a74
Skyler Dowdy <skylerwebdev@gmail.com>
2019-07-31 18:26:31 +0000

    dj-ad-sky 1426

--- rentech/src/components/render/Header.js
+++ rentech/src/components/render/Header.js
@@ -1,18 +1,18 @@
 import React from "react";
 import Logo from "../../img/rentech-logo-new.png";
 import { NavLink } from "react-router-dom";
+import LogOutPopOut from './LogOutPopOut'
+
 const Header = props => {
   return (
     <div className="profile-header">
         <img src={Logo} alt="RenTech Logo" className="logo" />
         <div className='float-right'>
-
-        <div className='link-container'>
-
-        <NavLink onClick= {()=> {localStorage.removeItem('token')}} className='login' to="/login">Logout</NavLink>
+<div className="link-container">
+         <LogOutPopOut />
         <NavLink className='login' to="/login">Login</NavLink>
         <NavLink to="createaccount">Create Account</NavLink>
-      </div>
+        </div>
 
       </div>
     </div>
