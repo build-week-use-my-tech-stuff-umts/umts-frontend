15242e245f32a1b5ce5aa54f50a38a445b4963b0
Skyler Dowdy <skylerwebdev@gmail.com>
2019-08-01 15:36:51 +0000

    dj-ad-sky 1136

--- rentech/src/components/render/ConfirmDelete.js
+++ rentech/src/components/render/ConfirmDelete.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Header, Button, Modal, Image } from "semantic-ui-react";
 //This Modal Component
 
-const ConfirmDelete = () => {
+const ConfirmDelete = (props) => {
+      console.log('deleteItemProps', props)
   return (
     <div className="edit-account">
       <Modal size='mini' trigger={<Button color="red" size='small' >Delete</Button>}>
         <Modal.Header>U Sure Bro?</Modal.Header>
         <Modal.Content>
-          <Button type='submit' positive>Fuck Yeah</Button>
+          <Button onClick = {() => {props.deleteItem(props.rentItem.id).then(() => {window.location.reload() })}}  type='submit' positive>Fuck Yeah</Button>
           <Button type='close' negative>Hell Nah</Button>
         </Modal.Content>
       </Modal>
@@ -17,3 +18,4 @@ const ConfirmDelete = () => {
 };
 
 export default ConfirmDelete;
+
