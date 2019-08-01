cac438e789e8439c94e045ceedbcb9f01ef25501
deejayeaster <deejayeaster@live.com>
2019-08-01 16:50:54 -0400

    styled login

--- rentech/src/components/render/LoginCard.js
+++ rentech/src/components/render/LoginCard.js
@@ -2,14 +2,16 @@ import React from "react";
 // import Header from "./Header.js";
 import LoginForm from "../forms/LoginForm.js";
 import Logo from "../../img/rentech-logo-new.png";
+import { Card } from "semantic-ui-react";
+
 export default function LoginCard(props) {
   return (
-    <div>
+    <Card className="login-card">
       <div className="center">
         <img src={Logo} alt="RenTech Logo" className="logo" />
       </div>
       <LoginForm props={props} />
-    </div>
+    </Card>
   );
 }
 //Some test code
