334824951b217de9c1e7cdc0e2cb57eda3576504
Orie Chinedu <oriechinedu@gmail.com>
2019-08-01 18:27:03 +0100

    Remove header and footer from login and register page

--- rentech/src/components/render/AppRouter.js
+++ rentech/src/components/render/AppRouter.js
@@ -3,24 +3,14 @@ import { Switch, Route } from "react-router-dom";
 import Marketplace from "./Marketplace";
 import MyTech from "./MyTech";
 import AccountInfo from "./AccountInfo";
-import LoginCard from './LoginCard';
 import CreateAcctCard from './CreateAcctCard'
 import PrivateRoute from './PrivateRoute'
 const AppRouter = () => {
   return (
     <div className="page-view bottom attached segment active tab">
-
-      <Switch>
         <PrivateRoute path="/marketplace" component={Marketplace} />
         <PrivateRoute path="/mytech" component={MyTech} />
         <PrivateRoute path="/account" component={AccountInfo} />
-
-        <Route path='/' exact render={props => <LoginCard {...props}/> } />
-
-        <Route path='/createaccount' render={props => <CreateAcctCard {...props} /> } />
-        <Route path='/login' render={props => <LoginCard {...props}/> } />
-
-      </Switch>
     </div>
   );
 };
--- rentech/src/index.js
+++ rentech/src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import { applyMiddleware, createStore } from "redux";
 import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import rootReducer from "./store/reducers/index";
 import App from "./App";
 
 import "./scss/index.scss";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
